ee021fe31d9238c33fa8a1ab10ad954e087b1fc6
brodieWest <brodie.west3@gmail.com>
2018-09-20 19:32:40 +0100

    added example of an button click event #11

--- src/main/java/javafx/sample/Controller.java
+++ src/main/java/javafx/sample/Controller.java
@@ -1,4 +1,16 @@
 package javafx.sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuItem;
+
+
 public class Controller {
+    @FXML
+    MenuItem load;
+
+    @FXML
+    protected void loadFile(ActionEvent event) {
+        System.out.println("working");
+    }
 }
