73105fac45e22a19e06a0d9b9d35dbe9802c6ca1
brodieWest <brodie.west3@gmail.com>
2018-09-20 15:05:06 +0100

    basic screen layout added top menu bar likely to be final high level layout #10

--- src/main/java/javafx/sample/Mainfx.java
+++ src/main/java/javafx/sample/Mainfx.java
@@ -6,15 +6,13 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.util.ArrayList;
-
 public class Mainfx extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("../../../resources/sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 800, 500));
         primaryStage.show();
     }
 
