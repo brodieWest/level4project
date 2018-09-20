54735cd6222b3f8e8164d206096f9819df79fee2
brodieWest <brodie.west3@gmail.com>
2018-09-20 20:04:01 +0100

    now building javafx components at runtime and adding them to the existing fxml on button press #11

--- src/main/java/javafx/sample/Controller.java
+++ src/main/java/javafx/sample/Controller.java
@@ -2,7 +2,9 @@ package javafx.sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.MenuItem;
+import javafx.scene.layout.VBox;
 
 
 public class Controller {
@@ -10,7 +12,12 @@ public class Controller {
     MenuItem load;
 
     @FXML
+    VBox simulationVBox;
+
+    @FXML
     protected void loadFile(ActionEvent event) {
-        System.out.println("working");
+        Label label = new Label();
+        label.setText("I'm an AND gate");
+        simulationVBox.getChildren().add(label);
     }
 }
