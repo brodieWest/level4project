4bd8abdb867bbff24f711f458d7fe9a77b592d29
brodieWest <brodie.west3@gmail.com>
2018-09-21 22:40:52 +0100

    connected the controllers #11

--- src/main/java/javafx/main/MainController.java
+++ src/main/java/javafx/main/MainController.java
@@ -1,6 +1,5 @@
 package javafx.main;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuItem;
@@ -10,13 +9,13 @@ import javafx.scene.layout.VBox;
 
 public class MainController extends BorderPane {
     @FXML
-    MenuItem load;
+    private MenuItem load;
 
     @FXML
-    VBox simulationVBox;
+    private VBox simulationVBox;
 
     @FXML
-    protected void loadFile(ActionEvent event) {
+    protected void loadFile() {
         Label label = new Label();
         label.setText("I'm an AND gate");
         simulationVBox.getChildren().add(label);
--- src/main/java/javafx/simulation/SimulationController.java
+++ src/main/java/javafx/simulation/SimulationController.java
@@ -1,4 +1,22 @@
 package javafx.simulation;
 
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+
 public class SimulationController {
+    @FXML
+    Node simulationGridPane;
+
+    @FXML
+    Button button;
+
+    @FXML
+    protected void buttonPress() {
+        if(button.getText().equals("loading...")) {
+            button.setText("I worked");
+        }    else {
+            button.setText("loading...");
+        }
+    }
 }
