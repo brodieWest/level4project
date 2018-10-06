04c798ea8b96cc8e1ca9431a1686c153581bb0c7
brodieWest <brodie.west3@gmail.com>
2018-10-06 20:28:12 +0100

    signals can move through wires #12

---
+++ src/test/java/javafx/wire/WireTest.java
@@ -0,0 +1,59 @@
+package javafx.wire;
+
+import model.Logic;
+import model.Port;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class WireTest {
+
+    private Wire wire;
+    private Logic inputLogic;
+    private Logic outputLogic;
+
+    @Before
+    public void setup() {
+        wire = new Wire();
+        Port input = new Port();
+        Port output = new Port();
+        wire.setInput(input);
+        wire.addOutput(output);
+        inputLogic = input.getLogic();
+        outputLogic = output.getLogic();
+    }
+
+    @Test
+    public void passSignal0() {
+        outputLogic.setValue(true);
+        inputLogic.setValue(false);
+        inputLogic.setUndefined(false);
+
+        wire.passSignal();
+
+        assertFalse(outputLogic.value());
+        assertFalse(outputLogic.isUndefined());
+    }
+
+    @Test
+    public void passSignal1() {
+        inputLogic.setValue(true);
+        inputLogic.setUndefined(false);
+
+        wire.passSignal();
+
+        assertTrue(outputLogic.value());
+        assertFalse(outputLogic.isUndefined());
+    }
+
+    @Test
+    public void passSignalUndefined() {
+        inputLogic.setValue(true);
+        inputLogic.setUndefined(true);
+
+        wire.passSignal();
+
+        assertTrue(outputLogic.isUndefined());
+    }
+}
